bb9f06a44bd4e5ce93a248622e471fe1ca246da3
noncog <108415765+noncog@users.noreply.github.com>
2022-07-19 18:50:16 -0400

    Created basic setup

--- rofi-borg.sh
+++ rofi-borg.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
+# rofi-borg options
+# =======================
+# rofi-borg location
+directory="$HOME/Projects/rofi-borg"
+# configured rofi commands
+rofi_command="rofi -theme $directory/configs/borg-rofi.rasi"
+rofi_error_command="rofi -theme $directory/configs/error.rasi"
+
+# borg options
+# ======================
+
+
+
+# rofi-borg
+# ======================
+
+# error message
+err_msg() {
+	$rofi_error_command -e "$1"
+}
+
+# testing error messasge
+err_msg "testing the command"
 
