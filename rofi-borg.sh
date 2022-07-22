265b396d7610a7361f6cf3f37ca231a55d57ea6f
noncog <108415765+noncog@users.noreply.github.com>
2022-07-22 12:41:54 -0400

    Added complete rofi-borg.sh

--- rofi-borg.sh
+++ rofi-borg.sh
@@ -1,39 +1,99 @@
 #!/bin/bash
 
-# rofi-borg options
-# =======================
-# rofi-borg location
-directory="$HOME/Projects/rofi-borg"
-# configured rofi commands
-rofi_command="rofi -theme $directory/configs/borg-rofi.rasi"
-rofi_error_command="rofi -theme $directory/configs/error.rasi"
+#===========#
+# user-vars # CHANGE
+#===========#
 
-# borg options
-# ======================
+directory="$HOME/Projects/rofi-borg"      # directory of rofi-borg
+# recommend $HOME/.config/rofi/rofi-borg
 
+downloads="$HOME/Downloads/borg-download" # downloads directory
+prompt_message="Borg"                     # rofi prompt message left of entry field
+log_count=7                               # amount of logs want to keep
+notifications="y"                         # set to n to disable 
+notifier="dunstify"                       # set to command for your notifications
+# caution - this command is evaluated. DO NOT put any dangerous commands in here
 
+#=============#
+# script-vars #  DONT CHANGE
+#=============#
 
-# rofi-borg
-# ======================
+scripts="$directory/scripts"                                   # dfirectory for menu item scripts
+logs="$directory/logs"                                         # directory of logs & tmp files
+config="${0##*/}"; config="${config%.*}.rasi"                  # get rofi config for script
+# config file titles must match associated script title
+
+rofi_command="rofi -theme $directory/configs/$config"          # rofi config for menu
+rofi_error_command="rofi -theme $directory/configs/error.rasi" # rofi config for error message
+
+#===========#
+# borg-vars # CHANGE
+#===========#
+
+export BORG_REPO='zh2361@zh2361.rsync.net:borg-backups'
+# use single quotes to prevent variable expansion
+export BORG_REMOTE_PATH="/usr/local/bin/borg1/borg1"    
+export BORG_PASSCOMMAND="cat $HOME/.borg-passphrase" 
+
+#============#
+# menu-items # CAN CUSTOMIZE
+#============#
+
+items=(
+	backup=" Backup"
+	list=" List"
+	download=" Download"
+	delete=" Delete"
+)
+
+scripts=(
+	backup="$scripts/backup_run.sh"
+	list="$scripts/backup_list.sh"
+	download="$scripts/backup_download.sh"
+	delete="$scripts/backup_delete.sh"
+)
+
+#===========#
+# rofi-borg # DONT CHANGE
+#===========#
+
+# create command to push/pipe menu into rofi
+push_menu() {
+	declare -A menu
+	declare -a order
+	# assemble menu items from items array
+	for item in "${items[@]}"; do
+		menu+=(["${item%=*}"]="${item#*=}")
+		order+=( "${item%=*}" )
+	done
+	for item in "${order[@]}"; do
+		echo "${menu["$item"]}"
+	done
+}
 
 # error message
 err_msg() {
 	$rofi_error_command -e "$1"
 }
 
-# menu items - to modify just add/remove items
-menu_items=(
-	backup="backup"
-	list="list"
-	download="download"
-	delete="delete"
-)
+# create logs directory
+if [ ! -d "${logs}" ]; then
+	mkdir -p "${logs}"
+fi
 
-for item in "${menu_items[@]}"; do
-	# this succesfully gets the list of variables
-	concat+="\$${item%=*}\n"
+# call rofi and return selection
+selection="$(push_menu | $rofi_command -p "$prompt_message" -dmenu)"
+
+# get index of selected command
+for i in "${!items[@]}"; do
+   if [[ "${items[$i]#*=}" = "$selection" ]]; then
+       index=$i
+   fi
 done
 
-items="${concat%\\*}"
-
-err_msg $items
+# execute command for selection
+if [[ -f "${scripts[index]#*=}" ]]; then
+	bash "${scripts[index]#*=}" $directory $notifications $notifier $logs $log_count $downloads
+else
+	err_msg "$selection script not found."
+fi
